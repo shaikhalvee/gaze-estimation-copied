ecd397f043c184d50b8e4b5f77da7120ff752ff4
Shaikh Islam <shaikhalvee@gmail.com>
2024-03-08 02:27:56 -0500

    code update

--- headpose.py
+++ headpose.py
@@ -20,31 +20,31 @@ threshold = .3
 
 mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(static_image_mode=False,
-    refine_landmarks=True,
-    max_num_faces=2,
-    min_detection_confidence=0.5)
+                                  refine_landmarks=True,
+                                  max_num_faces=2,
+                                  min_detection_confidence=0.5)
 cap = cv2.VideoCapture(0)
 
 face_3d = np.array([
-    [0.0, 0.0, 0.0],            # Nose tip
-    [0.0, -330.0, -65.0],       # Chin
-    [-225.0, 170.0, -135.0],    # Left eye left corner
-    [225.0, 170.0, -135.0],     # Right eye right corner
-    [-150.0, -150.0, -125.0],   # Left Mouth corner
-    [150.0, -150.0, -125.0]     # Right mouth corner
-    ], dtype=np.float64)
+    [0.0, 0.0, 0.0],  # Nose tip
+    [0.0, -330.0, -65.0],  # Chin
+    [-225.0, 170.0, -135.0],  # Left eye left corner
+    [225.0, 170.0, -135.0],  # Right eye right corner
+    [-150.0, -150.0, -125.0],  # Left Mouth corner
+    [150.0, -150.0, -125.0]  # Right mouth corner
+], dtype=np.float64)
 
 # Reposition left eye corner to be the origin
 leye_3d = np.array(face_3d)
-leye_3d[:,0] += 225
-leye_3d[:,1] -= 175
-leye_3d[:,2] += 135
+leye_3d[:, 0] += 225
+leye_3d[:, 1] -= 175
+leye_3d[:, 2] += 135
 
 # Reposition right eye corner to be the origin
 reye_3d = np.array(face_3d)
-reye_3d[:,0] -= 225
-reye_3d[:,1] -= 175
-reye_3d[:,2] += 135
+reye_3d[:, 0] -= 225
+reye_3d[:, 1] -= 175
+reye_3d[:, 2] += 135
 
 # Gaze scores from the previous frame
 last_lx, last_rx = 0, 0
@@ -58,11 +58,11 @@ while cap.isOpened():
 
     # To improve performance
     img.flags.writeable = False
-    
+
     # Get the result
     results = face_mesh.process(img)
     img.flags.writeable = True
-    
+
     # Convert the color space from RGB to BGR
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 
@@ -70,7 +70,7 @@ while cap.isOpened():
     face_2d = []
 
     if not results.multi_face_landmarks:
-      continue 
+        continue
 
     for face_landmarks in results.multi_face_landmarks:
         face_2d = []
@@ -80,52 +80,52 @@ while cap.isOpened():
 
             # Add the 2D coordinates to an array
             face_2d.append((x, y))
-        
+
         # Get relevant landmarks for headpose estimation
         face_2d_head = np.array([
-            face_2d[1],      # Nose
-            face_2d[199],    # Chin
-            face_2d[33],     # Left eye left corner
-            face_2d[263],    # Right eye right corner
-            face_2d[61],     # Left mouth corner
-            face_2d[291]     # Right mouth corner
+            face_2d[1],  # Nose
+            face_2d[199],  # Chin
+            face_2d[33],  # Left eye left corner
+            face_2d[263],  # Right eye right corner
+            face_2d[61],  # Left mouth corner
+            face_2d[291]  # Right mouth corner
         ], dtype=np.float64)
 
         face_2d = np.asarray(face_2d)
 
         # Calculate left x gaze score
-        if (face_2d[243,0] - face_2d[130,0]) != 0:
-            lx_score = (face_2d[468,0] - face_2d[130,0]) / (face_2d[243,0] - face_2d[130,0])
+        if (face_2d[243, 0] - face_2d[130, 0]) != 0:
+            lx_score = (face_2d[468, 0] - face_2d[130, 0]) / (face_2d[243, 0] - face_2d[130, 0])
             if abs(lx_score - last_lx) < threshold:
                 lx_score = (lx_score + last_lx) / 2
             last_lx = lx_score
 
         # Calculate left y gaze score
-        if (face_2d[23,1] - face_2d[27,1]) != 0:
-            ly_score = (face_2d[468,1] - face_2d[27,1]) / (face_2d[23,1] - face_2d[27,1])
+        if (face_2d[23, 1] - face_2d[27, 1]) != 0:
+            ly_score = (face_2d[468, 1] - face_2d[27, 1]) / (face_2d[23, 1] - face_2d[27, 1])
             if abs(ly_score - last_ly) < threshold:
                 ly_score = (ly_score + last_ly) / 2
             last_ly = ly_score
 
         # Calculate right x gaze score
-        if (face_2d[359,0] - face_2d[463,0]) != 0:
-            rx_score = (face_2d[473,0] - face_2d[463,0]) / (face_2d[359,0] - face_2d[463,0])
+        if (face_2d[359, 0] - face_2d[463, 0]) != 0:
+            rx_score = (face_2d[473, 0] - face_2d[463, 0]) / (face_2d[359, 0] - face_2d[463, 0])
             if abs(rx_score - last_rx) < threshold:
                 rx_score = (rx_score + last_rx) / 2
             last_rx = rx_score
 
         # Calculate right y gaze score
-        if (face_2d[253,1] - face_2d[257,1]) != 0:
-            ry_score = (face_2d[473,1] - face_2d[257,1]) / (face_2d[253,1] - face_2d[257,1])
+        if (face_2d[253, 1] - face_2d[257, 1]) != 0:
+            ry_score = (face_2d[473, 1] - face_2d[257, 1]) / (face_2d[253, 1] - face_2d[257, 1])
             if abs(ry_score - last_ry) < threshold:
                 ry_score = (ry_score + last_ry) / 2
             last_ry = ry_score
 
         # The camera matrix
         focal_length = 1 * img_w
-        cam_matrix = np.array([ [focal_length, 0, img_h / 2],
-                                [0, focal_length, img_w / 2],
-                                [0, 0, 1]])
+        cam_matrix = np.array([[focal_length, 0, img_h / 2],
+                               [0, focal_length, img_w / 2],
+                               [0, 0, 1]])
 
         # Distortion coefficients 
         dist_coeffs = np.zeros((4, 1), dtype=np.float64)
@@ -134,11 +134,9 @@ while cap.isOpened():
         _, l_rvec, l_tvec = cv2.solvePnP(leye_3d, face_2d_head, cam_matrix, dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
         _, r_rvec, r_tvec = cv2.solvePnP(reye_3d, face_2d_head, cam_matrix, dist_coeffs, flags=cv2.SOLVEPNP_ITERATIVE)
 
-
         # Get rotational matrix from rotational vector
         l_rmat, _ = cv2.Rodrigues(l_rvec)
         r_rmat, _ = cv2.Rodrigues(r_rvec)
-
 
         # [0] changes pitch
         # [1] changes roll
@@ -147,12 +145,12 @@ while cap.isOpened():
 
         # Adjust headpose vector with gaze score
         l_gaze_rvec = np.array(l_rvec)
-        l_gaze_rvec[2][0] -= (lx_score-.5) * x_score_multiplier
-        l_gaze_rvec[0][0] += (ly_score-.5) * y_score_multiplier
+        l_gaze_rvec[2][0] -= (lx_score - .5) * x_score_multiplier
+        l_gaze_rvec[0][0] += (ly_score - .5) * y_score_multiplier
 
         r_gaze_rvec = np.array(r_rvec)
-        r_gaze_rvec[2][0] -= (rx_score-.5) * x_score_multiplier
-        r_gaze_rvec[0][0] += (ry_score-.5) * y_score_multiplier
+        r_gaze_rvec[2][0] -= (rx_score - .5) * x_score_multiplier
+        r_gaze_rvec[0][0] += (ry_score - .5) * y_score_multiplier
 
         # --- Projection ---
 
@@ -167,18 +165,16 @@ while cap.isOpened():
         # Draw axis of rotation for left eye
         if draw_headpose:
             if draw_full_axis:
-                cv2.line(img, l_corner, tuple(np.ravel(l_axis[0]).astype(np.int32)), (200,200,0), 3)
-                cv2.line(img, l_corner, tuple(np.ravel(l_axis[1]).astype(np.int32)), (0,200,0), 3)
-            cv2.line(img, l_corner, tuple(np.ravel(l_axis[2]).astype(np.int32)), (0,200,200), 3)
+                cv2.line(img, l_corner, tuple(np.ravel(l_axis[0]).astype(np.int32)), (200, 200, 0), 3)
+                cv2.line(img, l_corner, tuple(np.ravel(l_axis[1]).astype(np.int32)), (0, 200, 0), 3)
+            cv2.line(img, l_corner, tuple(np.ravel(l_axis[2]).astype(np.int32)), (0, 200, 200), 3)
 
         if draw_gaze:
             if draw_full_axis:
-                cv2.line(img, l_corner, tuple(np.ravel(l_gaze_axis[0]).astype(np.int32)), (255,0,0), 3)
-                cv2.line(img, l_corner, tuple(np.ravel(l_gaze_axis[1]).astype(np.int32)), (0,255,0), 3)
-            cv2.line(img, l_corner, tuple(np.ravel(l_gaze_axis[2]).astype(np.int32)), (0,0,255), 3)
+                cv2.line(img, l_corner, tuple(np.ravel(l_gaze_axis[0]).astype(np.int32)), (255, 0, 0), 3)
+                cv2.line(img, l_corner, tuple(np.ravel(l_gaze_axis[1]).astype(np.int32)), (0, 255, 0), 3)
+            cv2.line(img, l_corner, tuple(np.ravel(l_gaze_axis[2]).astype(np.int32)), (0, 0, 255), 3)
 
-        
-    
         # Get left eye corner as integer
         r_corner = face_2d_head[3].astype(np.int32)
 
@@ -189,17 +185,15 @@ while cap.isOpened():
         # Draw axis of rotation for left eye
         if draw_headpose:
             if draw_full_axis:
-                cv2.line(img, r_corner, tuple(np.ravel(r_axis[0]).astype(np.int32)), (200,200,0), 3)
-                cv2.line(img, r_corner, tuple(np.ravel(r_axis[1]).astype(np.int32)), (0,200,0), 3)
-            cv2.line(img, r_corner, tuple(np.ravel(r_axis[2]).astype(np.int32)), (0,200,200), 3)
+                cv2.line(img, r_corner, tuple(np.ravel(r_axis[0]).astype(np.int32)), (200, 200, 0), 3)
+                cv2.line(img, r_corner, tuple(np.ravel(r_axis[1]).astype(np.int32)), (0, 200, 0), 3)
+            cv2.line(img, r_corner, tuple(np.ravel(r_axis[2]).astype(np.int32)), (0, 200, 200), 3)
 
         if draw_gaze:
             if draw_full_axis:
-                cv2.line(img, r_corner, tuple(np.ravel(r_gaze_axis[0]).astype(np.int32)), (255,0,0), 3)
-                cv2.line(img, r_corner, tuple(np.ravel(r_gaze_axis[1]).astype(np.int32)), (0,255,0), 3)
-            cv2.line(img, r_corner, tuple(np.ravel(r_gaze_axis[2]).astype(np.int32)), (0,0,255), 3)
-                
-
+                cv2.line(img, r_corner, tuple(np.ravel(r_gaze_axis[0]).astype(np.int32)), (255, 0, 0), 3)
+                cv2.line(img, r_corner, tuple(np.ravel(r_gaze_axis[1]).astype(np.int32)), (0, 255, 0), 3)
+            cv2.line(img, r_corner, tuple(np.ravel(r_gaze_axis[2]).astype(np.int32)), (0, 0, 255), 3)
 
     cv2.imshow('Head Pose Estimation', img)
 
--- old/dlib_eyes.py
+++ old/dlib_eyes.py
@@ -2,32 +2,35 @@ import cv2
 import dlib
 import numpy as np
 
+
 # Convert landmarks from dlib model to an array of x and y coordinates
 def get_landmarks(shape, dtype="int"):
-	landmarks = np.zeros((68, 2), dtype=dtype)
+    landmarks = np.zeros((68, 2), dtype=dtype)
 
-	# Get x and y coordinates from landmark model
-	for i in range(0, 68):
-		landmarks[i] = (shape.part(i).x, shape.part(i).y)
+    # Get x and y coordinates from landmark model
+    for i in range(0, 68):
+        landmarks[i] = (shape.part(i).x, shape.part(i).y)
 
-	return landmarks
+    return landmarks
+
 
 # Get iris center using moments
 def get_iris_center(thresholded_img, mid, img, right=False):
-    contours, _ = cv2.findContours(thresholded_img, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-    cx, cy = 0,0
+    contours, _ = cv2.findContours(thresholded_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    cx, cy = 0, 0
     try:
-        max_contour = max(contours, key = cv2.contourArea)
+        max_contour = max(contours, key=cv2.contourArea)
         M = cv2.moments(max_contour)
-        cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
         if right:
             cx += mid
         cv2.circle(img, (cx, cy), 4, (255, 0, 255), 2)
     except:
         pass
 
-    return (cx,cy)
+    return cx, cy
+
 
 # Fill/mask eyes
 def mask_eyes(mask, side):
@@ -35,6 +38,7 @@ def mask_eyes(mask, side):
     points = np.array(points, dtype=np.int32)
     mask = cv2.fillConvexPoly(mask, points, 255)
     return mask
+
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_68.dat')
@@ -50,16 +54,18 @@ thresholded_img = img.copy()
 cv2.namedWindow('image')
 kernel = np.ones((9, 9), np.uint8)
 
+
 def callback(x):
     pass
+
+
 cv2.createTrackbar('threshold', 'image', 0, 255, callback)
 
-while(True):
+while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     rects = detector(gray, 1)
     for rect in rects:
-        
         # Get landmarks
         shape = predictor(gray, rect)
         shape = get_landmarks(shape)
@@ -89,17 +95,17 @@ while(True):
 
         # Get left eye score
         lxscore = (lcx - shape[37][0]) / (shape[40][0] - shape[37][0])
-        cv2.putText(img, "{:02}".format(lxscore), (lcx, lcy-20), cv2.FONT_HERSHEY_COMPLEX, .25, (255,255,255), 1)
+        cv2.putText(img, "{:02}".format(lxscore), (lcx, lcy - 20), cv2.FONT_HERSHEY_COMPLEX, .25, (255, 255, 255), 1)
 
         # Get right eye score
         rxscore = (rcx - shape[43][0]) / (shape[46][0] - shape[43][0])
-        cv2.putText(img, "{:02}".format(rxscore), (rcx, rcy-20), cv2.FONT_HERSHEY_COMPLEX, .25, (255,255,255), 1)
+        cv2.putText(img, "{:02}".format(rxscore), (rcx, rcy - 20), cv2.FONT_HERSHEY_COMPLEX, .25, (255, 255, 255), 1)
 
     # Show image
     cv2.imshow('eyes', img)
     cv2.imshow("image", thresholded_img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-    
+
 cap.release()
 cv2.destroyAllWindows()
